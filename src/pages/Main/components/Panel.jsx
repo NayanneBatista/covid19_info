d7c2f0cf94cebbf306356825b87bdc060b5251f5
Nayanne Batista <nayanne.lb@gmail.com>
2021-03-04 19:08:15 -0300

    finished Panel

--- src/pages/Main/components/Panel.jsx
+++ src/pages/Main/components/Panel.jsx
@@ -1,7 +1,7 @@
 import React, { memo } from 'react'
-import RefreshIcon from 'assets/images/refresh.svg'
+// import RefreshIcon from '../../../assets/images/refresh.svg'
 import { Card, Typography, Button, Select, MenuItem } from '../../../components'
-import COUNTRIES from 'commons/constants/countries'
+import COUNTRIES from '../../../commons/constants/countries'
 import { CardPanelContentStyled, ItemStyled } from './style'
 
 const navigatorHasShare = navigator.share
@@ -18,20 +18,53 @@ function Panel ({ updateAt, onChange, data, country, getCovidData }) {
         </MenuItem>
     }
 
+    const textCovid19 = `País: ${country} - recuperados: ${recovered}`
+
+    const copyInfo = () => {
+        navigator.clipboard.writeText(textCovid19)
+    }
+
+    const shareInfo = () => {
+        navigator.share({
+            title: `Dados do Covid19 - ${country}`,
+            text: textCovid19,
+            url: 'https://covid19.dio.netlify.app/'
+        })
+    }
+
+    const renderShareButton = (
+        <div>
+            <Button variant="contained" color="primary" onClick={copyInfo}>
+                Compartilhar
+            </Button>
+        </div>
+    )
+
+    const renderCopyButton = (
+        <div>
+            <Button variant="contained" color="primary" onClick={copyInfo}>
+                Copiar
+            </Button>
+        </div>
+    )
+
     return (
         <Card>
             <CardPanelContentStyled>
                 <div>
-                    <Typography variant="h5" component="span" color="primary">COVID19</Typography>
-                    <Typography variant="h6" component="span" color="primary">Painel Coronavírus</Typography>
+                    <Typography variant="h5" component="span" color="primary">COVID19 </Typography>
+                    <Typography variant="h6" component="span" color="primary">Painel Coronavírus </Typography>
                     <Typography variant="body2" component="span" color="primary">Atualizado em: {updateAt}</Typography>
                     <div className="pt-2">
-                        <Select onChamge={onChange} value={country}>
+                        <Select onChange={onChange} value={country}>
                             {COUNTRIES.map(renderCountries)}
                         </Select>
                     </div>
                 </div>
+                {navigatorHasShare ? renderShareButton : renderCopyButton}
             </CardPanelContentStyled>
         </Card>
     )
 }
+
+export default memo(Panel)
